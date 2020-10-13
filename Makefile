cd383a5439caea0be56bc3e555c1c0ed5a88213b
mamepuro <beans.mamegame.laugh@icloud.com>
2020-10-14 05:14:09 +0900

    add linked list

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 gcd_iter: gcd_iter.o main_iter.o 
 gcd_euclid: gcd_euclid.o main_euclid.o
 gcd_recursive: gcd_recursive.o main_recursive.o
+linked_list: linked_list.o main_linked_list.o
---
+++ linked_list.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linked_list.h"
+
+Cell *head = NULL;
+
+void insert_cell(Cell *p, int d) {
+    Cell *new_cell = (Cell*)malloc(sizeof(Cell));
+    new_cell -> data = d;
+    new_cell -> next = p->next;
+    p->next = new_cell;
+}
+
+void insert_cell_top(int d) {
+  Cell *new_cell = (Cell*)malloc(sizeof(Cell));
+  new_cell->data = d;
+  new_cell->next = head;
+  head = new_cell;
+}
+
+void delete_cell(Cell *p) {
+    Cell *free_cell = p->next;
+    p-> next = free_cell-> next;
+    free(free_cell);
+}
+
+void delete_cell_top(void) {
+    Cell *head_cell = head;
+    head = head_cell->next;
+    free(head_cell);
+}
+
+void display(void) {
+    Cell *display_point = head;
+    while(display_point != NULL)
+    {
+        printf("%d",display_point->data);
+        printf(" ");
+        display_point = display_point->next;
+    }
+    printf("\n");
+}
